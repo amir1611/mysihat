e61e59432db5b7e217308bca0369a25c928b4e64
amir1611 <amirjr1611@gmail.com>
2024-09-04 17:28:33 +0800

    upgrade composer

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Database\QueryException; // Add this line
+use Illuminate\Database\QueryException;
 
 class RegisterController extends Controller
 {
