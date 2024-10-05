be17c4ba468c12724e12feb5d5c2d2d3e7ded0ce
yattish47 <yattish47@gmail.com>
2024-10-05 17:51:55 +0800

    Create migration file for appointment

---
+++ database/migrations/2024_10_05_095016_create_time_slots_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('time_slots', function (Blueprint $table) {
+            $table->id();
+            $table->integer('doctor_id');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->integer('duration')->nullable();
+            $table->string('status');
+            $table->string('day');
+            $table->integer('appointment_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('time_slots');
+    }
+};
