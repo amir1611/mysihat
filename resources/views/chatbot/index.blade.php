dae7f0a61aa4440debc9966620257604b6b50e95
yattish47 <yattish47@gmail.com>
2024-10-26 18:19:36 +0800

    Added tts feature to chat bot

--- resources/views/chatbot/index.blade.php
+++ resources/views/chatbot/index.blade.php
@@ -35,6 +35,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="{{ asset('js/chat.js') }}"></script>
+        <script src="{{ asset('js/tts.js')}}"></script>
 
     
         <div class="modal fade" id="bookingModal" tabindex="-1" aria-labelledby="bookingModalLabel" aria-hidden="true">
