91d4dae86af23ad80a42c5772d0bfc10233b8b71
sG <satiyaganes.sg@gmail.com>
2024-11-05 02:43:12 +0800

    Refactor select-modal.blade.php to use a default avatar image for doctors

--- resources/views/filament/custom-components/select-modal.blade.php
+++ resources/views/filament/custom-components/select-modal.blade.php
@@ -90,8 +90,8 @@
     @foreach ($doctors as $doctor)
         <div class="w-full max-w-md overflow-hidden bg-white rounded-lg shadow-lg">
             <figure>
-                <img class="object-cover object-center w-full h-64" src={{ Storage::url($doctor->avatar_url) }}
-                    alt="Doctor Image" />
+                <img class="object-cover object-center w-full h-64"
+                    src={{ asset('build/assets/Female Doctor Avatar.jpg') }} alt="Doctor Image" />
             </figure>
             <div class="p-6">
                 <h2 class="mb-2 text-2xl font-bold text-gray-800">{{ $doctor->name }}</h2>
