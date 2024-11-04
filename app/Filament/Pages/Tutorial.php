fe329949baf8cbcb88b9ec98630908cdde3aeb2f
sG <satiyaganes.sg@gmail.com>
2024-11-05 02:24:39 +0800

    Refactor Tutorial page to use HasPageShield trait and update navigation icon

--- app/Filament/Pages/Tutorial.php
+++ app/Filament/Pages/Tutorial.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Pages;
 
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use Filament\Pages\Page;
 
 class Tutorial extends Page
 {
+    use HasPageShield;
+
     protected static ?string $navigationIcon = 'heroicon-o-video-camera';
 
     protected static string $view = 'filament.pages.tutorial';
